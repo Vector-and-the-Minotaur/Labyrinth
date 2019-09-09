44f8a584695671fc29cd429fc5ab4a626704b1b8
arsummers <aliyasummers1@gmail.com>
2019-09-09 11:36:30 -0700

    basic movement

--- perseus.py
+++ perseus.py
@@ -10,21 +10,24 @@ def turn_right_at_obstacle():
 
         def right_turn():
             robot.behavior.turn_in_place(degrees(-90))
-            
+
+
+        # TODO: only move straight til he sees a wall.
+
         def move_straight():
             robot.behavior.drive_straight(distance_mm(300), speed_mmps(100))
 
         def say_hi():
             args = anki_vector.util.parse_command_args()
             with anki_vector.Robot(args.serial) as robot:
-                print('Vector says hello to the maze')
+                print('Vector says hello to the minotaur')
                 robot.behavior.say_text('Hello Minotaur')
 
         move_straight()
         right_turn()
         say_hi()
 
-    # register obstacle (by proximity?)
+    # register obstacle (by proximity?) --> robot_observed_object
         # if within right range, set motors to turn right
 
 if __name__ == "__main__":
