743dd3bca61e987123d397f98cc1311c78cc8b64
surfwalker <alexanderkreid@gmail.com>
2019-09-11 12:38:55 -0700

    Repeats turning right then going straight when it is 50mm from a wall three times.

--- hello-maze/turn_right_at_wall.py
+++ hello-maze/turn_right_at_wall.py
@@ -29,6 +29,30 @@ def main():
         robot.behavior.say_text("I go rights." )
         robot.behavior.turn_in_place(degrees(-90))
 
+        object_dist = robot.proximity.last_sensor_reading.distance
+        stopping_distance = object_dist.distance_mm - 50.0
+        print(f'Distance from object: {object_dist.distance_mm}')
+        print(f'Stopping distance: {stopping_distance}')
+        print("Go straight.")
+        robot.behavior.say_text('I go strights.')
+        robot.behavior.drive_straight(distance_mm(stopping_distance), speed_mmps(50))
+        robot.motors.stop_all_motors()
+        print("Turn right 90 degrees.")
+        robot.behavior.say_text("I go rights.")
+        robot.behavior.turn_in_place(degrees(-90))
+
+        object_dist = robot.proximity.last_sensor_reading.distance
+        stopping_distance = object_dist.distance_mm - 50.0
+        print(f'Distance from object: {object_dist.distance_mm}')
+        print(f'Stopping distance: {stopping_distance}')
+        print("Go straight.")
+        robot.behavior.say_text('I go strights.')
+        robot.behavior.drive_straight(distance_mm(stopping_distance), speed_mmps(50))
+        robot.motors.stop_all_motors()
+        print("Turn right 90 degrees.")
+        robot.behavior.say_text("I go rights." )
+        robot.behavior.turn_in_place(degrees(-90))
+
         # robot.behavior.drive_on_charger()
         
 if __name__ == "__main__":
